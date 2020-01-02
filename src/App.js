06be2f938a98461fbb369c8ab5491cac509d0707
halfmoonpdh <enqn0903@naver.com>
2020-01-02 22:28:44 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled, { createGlobalStyle, ThemeProvider } from "styled-components";
+import theme from "./theme";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const GlobalStyle = createGlobalStyle`
+  body{
+    padding: 0;
+    margin: 0;
+  }
+`;
+
+const Card = styled.div`
+  background-color: white;
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: #bdc3c7;
+  ${Card} {
+    background-color: blue;
+  }
+`;
+
+const Button = styled.button`
+  border-radius: 30px;
+  padding: 25px 15px;
+  background-color: ${props => props.theme.successColor};
+`;
+
+class App extends React.Component {
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <Container>
+          <GlobalStyle />
+          <Form />
+        </Container>
+      </ThemeProvider>
+    );
+  }
 }
+
+const Form = () => (
+  <Card>
+    <Button>hello</Button>
+  </Card>
+);
 
 export default App;
